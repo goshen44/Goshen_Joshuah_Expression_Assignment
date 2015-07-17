e282b74791f6b0e833398e5815bef436eada1b0e
goshen44 <jpgoshen@gmail.com>
2015-07-16 21:28:38 -0400

    Second Commit

--- js/script.js
+++ js/script.js
@@ -11,3 +11,16 @@ console.log(date);
 console.log(assignment);
 //I am going to show you how to properly snorkel based on the flowchart that I created.
 //It is going to follow the processes and the decisions to the overall goal of explore and enjoying!
+//When first snorkeling you have to put on your mask and snorkel, that are going to represent the variable a
+var a=2;
+//Next you have to put on your two fins, I am going to add them to the mask and snorkel.
+a=a+2;
+console.log(a);
+//Now that you have your mask, snorkel and fins on, you are now ready to jump off the boat. You can either jump or not!
+var jump= true;
+console.log(jump);
+var noJump=(true);
+console.log(noJump);
+//If you don't jump then you stay on the boat and the end result is No Adventure, No Fun!
+
+
