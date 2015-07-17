e7900290cbdab59c7ed020864740877038cf895f
goshen44 <jpgoshen@gmail.com>
2015-07-16 20:51:37 -0400

    First Commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//My name  is Josh Goshen, I am in term 1507. The date is July 16th, 2015 and this is for the Expressions Assignment.
+var firstName="Josh";
+var lastName="Goshen";
+var term= "1507";
+var date= "July 16th, 2015";
+var assignment= "Expressions Assignment";
+var fullName= firstName + " " + lastName;
+console.log(fullName);
+console.log(term);
+console.log(date);
+console.log(assignment);
+//I am going to show you how to properly snorkel based on the flowchart that I created.
+//It is going to follow the processes and the decisions to the overall goal of explore and enjoying!
